bce591b8f95287113df4facd8c399cc00ca37878
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-01-17 16:18:41 +0100

    the first task has been implemented

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerId = null;
+
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStart() {
+    timerId = setInterval(() => { getBgColor() }, 1000)
+    // setInterval(() => { getBgColor() }, 1000)
+    
+    btnStart.toggleAttribute('disabled');
+}
+
+function onStop() {
+    clearInterval(timerId);
+
+    btnStart.removeAttribute('disabled');
+}
+
+function getBgColor() {
+    const newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor;
+}
+
+
 
